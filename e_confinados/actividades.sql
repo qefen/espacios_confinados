0758896137e7d780f0509d21f81e90fa0913ef7c
jabin753 <jabin753@gmail.com>
2019-09-19 12:34:52 -0500

    Timestamp Format entrada-salida removed

--- e_confinados/actividades.sql
+++ e_confinados/actividades.sql
@@ -1,17 +1,9 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         10.1.37-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win32
--- HeidiSQL Versión:             9.4.0.5125
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Volcando estructura para tabla e_confinados.actividades
 CREATE TABLE IF NOT EXISTS `actividades` (
   `idActividad` int(11) NOT NULL AUTO_INCREMENT,
   `nombreActividad` varchar(255) DEFAULT NULL,
@@ -25,7 +17,6 @@ CREATE TABLE IF NOT EXISTS `actividades` (
   PRIMARY KEY (`idActividad`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- e_confinados/trabajadores.sql
+++ e_confinados/trabajadores.sql
@@ -1,31 +1,22 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         10.1.37-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win32
--- HeidiSQL Versión:             9.4.0.5125
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Volcando estructura para tabla e_confinados.trabajadores
 CREATE TABLE IF NOT EXISTS `trabajadores` (
   `idTrabajador` int(11) NOT NULL AUTO_INCREMENT,
   `idActividad` int(11) NOT NULL,
   `numeroTrabajador` varchar(50) DEFAULT NULL,
   `nombreTrabajador` varchar(50) DEFAULT NULL,
-  `horaEntradaTrabajador` timestamp NULL DEFAULT NULL,
-  `horaSalidaTrabajador` timestamp NULL DEFAULT NULL,
+  `horaEntradaTrabajador` varchar(20) DEFAULT NULL,
+  `horaSalidaTrabajador` varchar(20) DEFAULT NULL,
   `estadoTrabajador` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`idTrabajador`),
   KEY `trabajadores_actividades` (`idActividad`),
   CONSTRAINT `trabajadores_actividades` FOREIGN KEY (`idActividad`) REFERENCES `actividades` (`idActividad`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- La exportación de datos fue deseleccionada.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
